b119f4bc3dda105f7d25cc6fdacc7e20d9ab48a8
Nevins Bartolomeo <nbartolomeo@carbonblack.com>
2017-01-13 12:08:32 -0500

    updating write_bucket_permission_validator to use plugin logger

--- lib/logstash/outputs/s3/write_bucket_permission_validator.rb
+++ lib/logstash/outputs/s3/write_bucket_permission_validator.rb
@@ -7,13 +7,18 @@ module LogStash
   module Outputs
     class S3
       class WriteBucketPermissionValidator
+        attr_reader :logger
 
-        def self.valid?(bucket_resource)
+        def initialize(logger)
+          @logger = logger
+        end
+
+        def valid?(bucket_resource)
           begin
             upload_test_file(bucket_resource)
             true
           rescue StandardError => e
-            @logger.error("Error validating bucket write permissions!",
+            logger.error("Error validating bucket write permissions!",
               :message => e.message,
               :class => e.class.name
               )
@@ -22,7 +27,7 @@ module LogStash
         end
 
         private
-        def self.upload_test_file(bucket_resource)
+        def upload_test_file(bucket_resource)
           generated_at = Time.now
 
           key = "logstash-programmatic-access-test-object-#{generated_at}"
