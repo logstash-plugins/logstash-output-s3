6d6ef36fb578462a16dda0bf311b99a1a55b3c0d
Manolis Mavrofidis <mmavrofides@gmail.com>
2017-08-29 17:53:10 +0100

    Fix for spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,20 @@ require_relative "supports/helpers"
 require "logstash/logging/logger"
 
 LogStash::Logging::Logger::configure_logging("debug") if ENV["DEBUG"]
+
+unless LogStash::Environment.const_defined?(:LOGSTASH_HOME)
+  LogStash::Environment::LOGSTASH_HOME = File.expand_path("../../", __FILE__)
+end
+
+# temporary fix to have the spec pass for an urgen mass-publish requirement.
+# cut & pasted from the same tmp fix in the grok spec
+# see https://github.com/logstash-plugins/logstash-filter-grok/issues/72
+# this needs to be refactored and properly fixed
+module LogStash::Environment
+  # also :pattern_path method must exist so we define it too
+  unless self.method_defined?(:pattern_path)
+    def pattern_path(path)
+      ::File.join(LOGSTASH_HOME, "patterns", path)
+    end
+  end
+end
