5dba397f1d0773a853a2924f8f4c0735a192e89e
Rob Bavey <rob.bavey@elastic.co>
2018-10-25 10:39:23 -0400

    Only upload temporary files to s3 when they are > 0 bytes
    Fixes #195

--- spec/integration/restore_from_crash_spec.rb
+++ spec/integration/restore_from_crash_spec.rb
@@ -11,29 +11,56 @@ describe "Restore from crash", :integration => true do
 
   let(:number_of_files) { 5 }
   let(:dummy_content) { "foobar\n" * 100 }
+  let(:factory) { LogStash::Outputs::S3::TemporaryFileFactory.new(prefix, tags, "none", temporary_directory)}
 
   before do
     clean_remote_files(prefix)
-    # Use the S3 factory to create mutliples files with dummy content
-    factory = LogStash::Outputs::S3::TemporaryFileFactory.new(prefix, tags, "none", temporary_directory)
+  end
 
-    # Creating a factory always create a file
-    factory.current.write(dummy_content)
-    factory.current.fsync
 
-    (number_of_files - 1).times do
-      factory.rotate!
+  context 'with a non-empty tempfile' do
+    before do
+      # Creating a factory always create a file
       factory.current.write(dummy_content)
       factory.current.fsync
+
+      (number_of_files - 1).times do
+        factory.rotate!
+        factory.current.write(dummy_content)
+        factory.current.fsync
+      end
+    end
+    it "uploads the file to the bucket" do
+      subject.register
+      try(20) do
+        expect(bucket_resource.objects(:prefix => prefix).count).to eq(number_of_files)
+        expect(Dir.glob(File.join(temporary_directory, "*")).size).to eq(0)
+        expect(bucket_resource.objects(:prefix => prefix).first.acl.grants.collect(&:permission)).to include("READ", "WRITE")
+      end
     end
   end
 
-  it "uploads the file to the bucket" do
-    subject.register
-    try(20) do
-      expect(bucket_resource.objects(:prefix => prefix).count).to eq(number_of_files)
-      expect(Dir.glob(File.join(temporary_directory, "*")).size).to eq(0)
-      expect(bucket_resource.objects(:prefix => prefix).first.acl.grants.collect(&:permission)).to include("READ", "WRITE")
+  context 'with an empty tempfile' do
+    before do
+      factory.current
+      factory.rotate!
+    end
+
+    it "should remove the temporary file" do
+      expect(Dir.glob(::File.join(temporary_directory, "**", "*")).size).to be > 0
+      subject.register
+      puts Dir.glob(::File.join(temporary_directory, "**", "*"))
+      expect(Dir.glob(::File.join(temporary_directory, "**", "*")).size).to eq(0)
+    end
+
+    it "should not upload the file to the bucket" do
+      expect(bucket_resource.objects(:prefix => prefix).count).to eq(0)
+      expect(Dir.glob(::File.join(temporary_directory, "**", "*")).size).to be > 0
+      subject.register
+
+      # Sleep to give enough time for plugin upload to s3 if it attempts to upload empty temporary file to S3
+      sleep 5
+      expect(bucket_resource.objects(:prefix => prefix).count).to eq(0)
     end
   end
 end
