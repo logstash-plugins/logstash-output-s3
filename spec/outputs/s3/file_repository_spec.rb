ba27986e154b72ac0030629f8872d59de2c7e250
Karol Bucek <kares@users.noreply.github.com>
2022-06-14 10:31:54 +0200

    Test: work-around issue with JRuby 9.3.4 (#247)

--- spec/outputs/s3/file_repository_spec.rb
+++ spec/outputs/s3/file_repository_spec.rb
@@ -88,8 +88,8 @@ describe LogStash::Outputs::S3::FileRepository do
 
   it "returns all available keys" do
     subject.get_file(prefix_key) { |file| file.write("something") }
-    expect(subject.keys.toArray).to include(prefix_key)
-    expect(subject.keys.toArray.size).to eq(1)
+    expect(subject.keys).to include(prefix_key)
+    expect(subject.keys.to_a.size).to eq(1)
   end
 
   it "clean stale factories" do
